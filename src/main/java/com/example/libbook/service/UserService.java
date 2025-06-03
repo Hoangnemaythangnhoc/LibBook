2920cfce239cc82dd68ece2d332f97f2c370b4fe
thienlnde1512 <GGlk8thien@gmail.com>
2025-06-04 05:57:57 +0700

    done UI 70%

--- src/main/java/com/example/libbook/service/UserService.java
+++ src/main/java/com/example/libbook/service/UserService.java
@@ -1,4 +1,11 @@
 package com.example.libbook.service;
 
+import com.example.libbook.dto.UserDTO;
+
 public interface UserService {
+    public boolean isEmailExist(String email);
+    boolean createAccount(UserDTO userDTO);
+    UserDTO checkLogin (String email,String pass);
+    UserDTO getUserByEmail(String email);
+
 }
--- src/main/java/com/example/libbook/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/libbook/service/impl/UserServiceImpl.java
@@ -1,4 +1,39 @@
 package com.example.libbook.service.impl;
 
-public class UserServiceImpl {
+import com.example.libbook.dto.UserDTO;
+import com.example.libbook.repository.UserRepository;
+import com.example.libbook.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public boolean createAccount(UserDTO userDTO) {
+        // Kiểm tra email đã tồn tại
+        if (isEmailExist(userDTO.getEmail())) {
+            throw new IllegalArgumentException("Email already exists!");
+        }
+        // Lưu tài khoản
+        return userRepository.createAccount(userDTO);
+    }
+
+    @Override
+    public UserDTO checkLogin(String email, String pass) {
+        return userRepository.checkLogin(email, pass);
+    }
+
+    @Override
+    public UserDTO getUserByEmail(String email) {
+        return userRepository.getUserByEmail(email);
+    }
+
+    @Override
+    public boolean isEmailExist(String email) {
+        return userRepository.isEmailExist(email);
+    }
 }
