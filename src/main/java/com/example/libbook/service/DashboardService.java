f70d4c0c63803f60bb32dee7d10bf84dd97d2cad
bao <thebao122004@gmail.com>
2025-07-29 11:23:42 +0700

    fix final

--- src/main/java/com/example/libbook/service/DashboardService.java
+++ src/main/java/com/example/libbook/service/DashboardService.java
@@ -4,6 +4,9 @@ import com.example.libbook.dto.DashboardData;
 import com.example.libbook.dto.MultiChartData;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 public interface DashboardService {
     DashboardData getDashboardData(String timeType);
